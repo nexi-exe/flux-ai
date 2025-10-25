d6b8453bef87e89f8eb1e3504e09a986b2604c1a
nexi-exe <darshanmantaganidm17@gmail.com>
2025-10-25 17:13:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 import os
-
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-MONGODB_URI = os.getenv("MONGODB_URI")
-OWNER_ID = int(os.getenv("OWNER_ID"))
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-LAVALINK_HOST = os.getenv("LAVALINK_HOST", "localhost")
-LAVALINK_PORT = int(os.getenv("LAVALINK_PORT", "2333"))
-LAVALINK_PASSWORD = os.getenv("LAVALINK_PASSWORD", "youshallnotpass")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
